a19f7b62611cfa1ce95ae73a88a5cba726d4a8c0
adel-yksh <116913836+adel-yksh@users.noreply.github.com>
2023-04-19 12:19:10 +0300

    delete comment

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ browser = webdriver.Chrome()
 
 browser.get("http://suninjuly.github.io/wait2.html")
 
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
 button = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.ID, "verify"))
     )
